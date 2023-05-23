e741cc1f7eff4e912bf580d2cca8389f968e75ce
F.G.5510 <130413112+Fernanda5510@users.noreply.github.com>
2023-05-23 20:19:05 -0300

    Update Program.cs(Comentado)

--- Program.cs
+++ Program.cs
@@ -3,15 +3,15 @@ using System.Globalization;  //Biblioteca que permite a convercao de um tipo de 
 
 namespace Course  //Local do programa;
 {
-    public class Triangulo  //Classe que possibilita a fabricacao dos objetos triangulos
+    public class Triangulo  //Classe triangulo que contera os atributos e metodos para seus objetos;
 
 
     {
-        public double A { get; set; } //Variavel que disponivel
-        public double B { get; set; }
-        public double C { get; set; }
+        public double A { get; set; } //Variavel do triangulo que ira conter o valor deste lado;
+        public double B { get; set; } //Variavel do triangulo que ira conter o valor deste lado;
+        public double C { get; set; } //Variavel do triangulo que ira conter o valor deste lado;
 
-        public double CalcularArea()
+        public double CalcularArea()//Metodo que ira calcular a area;
         {
             double p = (A + B + C) / 2.0; //Calculo do p que é a metade do perimetro do triangulo e necessario para calcular a area;
             double area = Math.Sqrt(p * (p - A) * (p - B) * (p - C)); // Calculo da area;
@@ -23,43 +23,43 @@ namespace Course  //Local do programa;
 
 
 
-    public class program
+    public class program //Classe que contem o codigo principal
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args)//Funcao principal
         {
-            Triangulo x = LerTriangulo("X");
-            Triangulo y = LerTriangulo("Y");
+            Triangulo x = LerTriangulo("x"); // cria x que é objeto da classe triangulo e recebera o valor de seus atributos A, B e C por meio da funcao LerTriangulo;
+            Triangulo y = LerTriangulo("y"); // cria y que é objeto da classe triangulo e recebera o valor de seus atributos A, B e C por meio da funcao LerTriangulo;
 
-            double areaX = x.CalcularArea();
-            double areaY = y.CalcularArea();
+            double areaX = x.CalcularArea(); //a variavel areaX recebera o valor retornado do metodo CalcularArea() do objeto x;
+            double areaY = y.CalcularArea(); //a variavel areaY recebera o valor retornado do metodo CalcularArea() do objeto y;
 
-            Console.WriteLine("Area de X =" + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Area de Y =" + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de x =" + areaX.ToString("F4", CultureInfo.InvariantCulture)); // mostra a area de X para o usuario converntendo seu valor para uma string e arrendondando seu valor para ate quatro numeros apos a virgula;
+            Console.WriteLine("Area de y =" + areaY.ToString("F4", CultureInfo.InvariantCulture)); // mostra a area de Y para o usuario converntendo seu valor para uma string e arrendondando seu valor para ate quatro numeros apos a virgula;
 
-            if (areaX > areaY)
+            if (areaX > areaY) //Caso a area de X seja maior que a de Y;
             {
-                Console.WriteLine("Maior area: X");
+                Console.WriteLine("Maior area: x"); //Relatar a area maior para o usuario;
             }
-            else if (areaX < areaY)
+            else if (areaX < areaY)/ /Caso a area de X seja menor que a de Y;
             {
-                Console.WriteLine("Maior area: Y");
+                Console.WriteLine("Maior area: y"); //Relatar a area maior para o usuario;
             }
-            else
+            else //caso contrario;
             {
-                Console.WriteLine("Areas iguais.");
+                Console.WriteLine("Areas iguais."); //Relatar  que as areas sao iguais para o usuario;
             }
         }
 
-        public static Triangulo LerTriangulo(string nomeTriangulo)
+        public static Triangulo LerTriangulo(string nomeTriangulo)//Funcao que ira fazer o calculo e retonar a area. Recebe como atributos uma array composta por doubles. Pertence a classe triangulo;
         {
-            Triangulo triangulo = new Triangulo();
+            Triangulo triangulo = new Triangulo(); //cria um objeto da classe triangulo;
 
-            Console.WriteLine("Entre as medidas do triangulo" + nomeTriangulo + ":");
-            triangulo.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            triangulo.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            triangulo.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre as medidas do triangulo" + nomeTriangulo + ":"); //Interacao com o usuario pedindo a medida do triangulo;
+            triangulo.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //O valor informado pelo usuario e convertido para double e amazenado pelo objeto em seu atributo A;
+            triangulo.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //O valor informado pelo usuario e convertido para double e amazenado pelo objeto em seu atributo B;
+            triangulo.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //O valor informado pelo usuario e convertido para double e amazenado pelo objeto em seu atributo C;
 
-            return triangulo;
+            return triangulo; //Retona as informacoes atribuidas no objeto triangulo;
         }
 
     }
